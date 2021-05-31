d52986c76ff9a5ef62dac4e22f40573467a9931b
Carl <carlbrainegebe@gmail.com>
2021-05-31 13:09:30 -0500

    commit tree 054ad62dafecee2f92923e9bdd56fa607ec2b6c6

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
 // Add your code here
+function submitData(userName, userEmail) {
+
+//the data were trying to add to the server via post
+const formData = {
+    name: userName,
+    email: userEmail
+};
+const configurationObj = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json", 
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+
+}; 
+
+return fetch("http://localhost:3000/users", configurationObj)
+.then(  (response) => {return response.json();}  )
+//get id from response and append its value to the DOM
+.then(  (returnData) => {
+console.log(returnData);
+const idValue = returnData.id;
+//now append to body
+const body = document.querySelector("body");
+const p = document.createElement("p");
+p.textContent = idValue;
+body.appendChild(p);
+}  )
+.catch(  (error) => {
+//create variable to store error message
+    const errorMessage = error.message;
+    //create p element to put error message in
+    const p = document.createElement("p");
+    //set error message as textContent of p
+    p.textContent = errorMessage;
+    //select body element which p will be appended to
+    const body = document.querySelector("body");
+    //append the p to the body
+    body.appendChild(p);
+} );
+
+
+
+
+
+
+
+}
